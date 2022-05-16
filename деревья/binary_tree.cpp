430166811991d9204e025fe70ca0ad0d9c2bbfd2
mishasurmach <91309866+mishasurmach@users.noreply.github.com>
2022-05-16 19:03:37 +0300

    добавил красивый вывод через обход в ширину.

--- деревья/binary_tree.cpp
+++ деревья/binary_tree.cpp
@@ -29,11 +29,10 @@ node *input(){
 
     while (n != 0){
         std::cin >> n;
-
         if (n == 0){
             break;
         }
-
+        
         num = root;
         back = num;
         while (num){
@@ -66,9 +65,50 @@ node *input(){
     return root;
 }
 
+int height(node *root){
+    if (root == nullptr)
+        return NULL;
+    
+    int left_height = height(root -> left), right_height = height(root -> right);
+    return std::max(left_height, right_height) + 1;
+}
+
+void make_space(int a, int b, int c){
+    for (int i = 0; i < c - a + b; i++)
+        std::cout << " ";
+}
+
+void defl(node *root, int level, int counter){
+    int tree_height = height(root);
+
+    if (root == nullptr){
+        make_space(level, counter, tree_height);
+        return;
+    }
+    if (level == 0){
+        make_space(level, counter, tree_height);
+        std::cout << root->digit;
+    }
+    else if (level > 0){
+        defl(root -> left, level - 1, counter);
+        make_space(level, counter, tree_height);
+        defl(root -> right, level - 1, counter);
+    }
+}
+
+void outreach(node *root){
+    int root_height = height(root);
+    for(int i = 0; i < root_height; i++){
+        for(int j = 0; j < root_height - i; j++)
+            std::cout << " ";
+        defl(root, i, i);
+        std::cout << std::endl;
+    }
+}
+
 int main(){
     node *root = input();
-    output(root);
+    outreach(root);
 
     return 0;
 }
